2a7059d061bee2b65e0034f91e1a1320eb809fcd
Ey Pacha! <pachaguionbajo@gmail.com>
2025-11-25 13:39:48 -0300

    feat: replace CaptchaPopup with dynamic captcha component rendering in CaptchaBrowser

--- src/components/browser-templates/CaptchaBrowser.vue
+++ src/components/browser-templates/CaptchaBrowser.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="flex flex-col align-items-center justify-center gap-4">
     <h2 class="text-center font-bold text-lg">BBA Verificación de seguridad</h2>
-    <CaptchaPopup @close="closePopup" />
+    <component :is="captchaComponent" @submit="closePopup" />
   </div>
 </template>
 
 <script setup>
-import CaptchaPopup from '../CaptchaPopup.vue';
-import { defineEmits } from 'vue';
+import { defineProps, defineEmits } from 'vue';
 const emit = defineEmits(['close']);
+const props = defineProps({ captchaComponent: Object });
+const captchaComponent = props.captchaComponent;
+console.log('[DEBUG] CaptchaBrowser captchaComponent:', captchaComponent);
 function closePopup() {
   emit('close');
 }
